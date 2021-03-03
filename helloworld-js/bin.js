f5ebb984849109d865e90b8d28ac90aaa14425a3
James Harmison <jharmison@redhat.com>
2021-03-03 17:25:22 -0500

    Added arg for changing listen address

--- helloworld-js/bin.js
+++ helloworld-js/bin.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 var server = require('./lib/server')
+var args = process.argv.slice(2);
 
-const listen = '127.0.0.1';
+if (args.length == 0) {
+    var listen = '127.0.0.1';
+} else {
+    var listen = args[0];
+}
 const port = 3000;
 
 server.listen(port, listen, () => {
