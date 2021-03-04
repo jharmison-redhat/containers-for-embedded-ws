599441e8be30daf649f501498302219f1a7cf943
James Harmison <jharmison@redhat.com>
2021-03-03 23:34:55 -0500

    Changed Rust example to use master

--- helloworld-rs/src/main.rs
+++ helloworld-rs/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 use nix::unistd;
 
@@ -8,9 +6,11 @@ fn index() -> String {
     let mut buf = [0u8; 64];
     let hostname_cstr = unistd::gethostname(&mut buf).expect("Failed getting hostname");
     let hostname = hostname_cstr.to_str().expect("Hostname wasn't valid UTF-8");
-    format!("Hello, world, from {}!", hostname)
+    format!("Hello, world, from {}!\n", hostname)
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[launch]
+fn helloworld() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
 }
